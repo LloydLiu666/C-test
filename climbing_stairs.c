665fd28bc088c476034b55bb96f97fc3a54d11fd
LloydLiu666 <59760145+LloydLiu666@users.noreply.github.com>
2020-09-18 09:41:48 +0800

    Update climbing_stairs.c

--- climbing_stairs.c
+++ climbing_stairs.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 int solution(int n){
 	if(n<=0){
-		return 0; //不合法的台阶数
+		return 0; //Illegal number of stairs
 	}
 	switch(n){
 		case 1: return 1;
 		case 2: return 2;
 		default:{
-			//int sum = 2;
-			//for (int i = 1; i < n - 1; i++){
-				//sum += solution(i);
-			//}
-			//return sum;
 			return solution(n-1)+solution(n-2);
-			//solution(n),以下简称s(n)
-			//s(n)=s(n-2)+s(n-3)+s(n-4)+...+s(1)+2
-			//s(n)=s(n-2)+[s(n-3)+s(n-4)+...+s(1)+2]
-			//s(n)=s(n-2)+s(n-1)
 		}		
 	}
 	
@@ -24,7 +15,7 @@ int solution(int n){
 }
 int main(void){
 	int s;
-	printf("请输入台阶数：");
+	printf("Please input the number of stairs:");
 	scanf("%d",&s);
-	printf("走法共有%d种。\n",solution(s));
+	printf("There are %d ways of walking to the top.\n",solution(s));
 }
